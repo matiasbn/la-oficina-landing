7ef0dfa871842b0de7a59edf84f4896a4f1aeff2
Matías Barrios <matias.barriosn@gmail.com>
2024-10-17 17:11:23 -0300

    delay

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 import "./App.css";
 import logo from "./la_oficina.jpg"; // Using the local logo image
 
-// Función para rastrear los eventos de clic
-const trackButtonClick = (label) => {
+// Función para rastrear los eventos de clic y agregar un delay
+const trackButtonClick = (label, url) => {
+  // Enviar evento a Google Analytics
   window.gtag("event", "click", {
     event_category: "Button",
     event_label: label,
     value: 1,
   });
+
+  // Agregar un retraso de 300 ms antes de redirigir al enlace
+  setTimeout(() => {
+    window.open(url, "_blank");
+  }, 3000);
 };
 
 function App() {
@@ -21,76 +27,91 @@ function App() {
       </p>
       <div className="link-container">
         <a
-          href="https://laoficina.evently.cl/concrete-jungle-vol-1"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
           className="link-button"
-          onClick={() => trackButtonClick("Entradas Concrete Jungle Vol 1")}
+          onClick={() =>
+            trackButtonClick(
+              "Entradas Concrete Jungle Vol 1",
+              "https://laoficina.evently.cl/concrete-jungle-vol-1"
+            )
+          }
         >
           Entradas Concrete Jungle Vol 1,
           <br />
           Fiesta de La Oficina
         </a>
         <a
-          href="https://docs.google.com/spreadsheets/d/1UXMq8TsTPNdkqb6PV1JOFQkmWyhkEFXH5RSqP_sMlak/edit?gid=0#gid=0"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
           className="link-button"
-          onClick={() => trackButtonClick("Calendario")}
+          onClick={() =>
+            trackButtonClick(
+              "Calendario",
+              "https://docs.google.com/spreadsheets/d/1UXMq8TsTPNdkqb6PV1JOFQkmWyhkEFXH5RSqP_sMlak/edit?gid=0#gid=0"
+            )
+          }
         >
           Calendario
         </a>
         <a
-          href="https://tiktok.com/@laoficina.music"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
           className="link-button"
-          onClick={() => trackButtonClick("TikTok")}
+          onClick={() =>
+            trackButtonClick("TikTok", "https://tiktok.com/@laoficina.music")
+          }
         >
           Tik Tok
         </a>
         <a
-          href="https://instagram.com/laoficina.music"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
           className="link-button"
-          onClick={() => trackButtonClick("Instagram")}
+          onClick={() =>
+            trackButtonClick(
+              "Instagram",
+              "https://instagram.com/laoficina.music"
+            )
+          }
         >
           Instagram
         </a>
         <a
-          href="https://youtube.com/@laoficina_music"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
           className="link-button"
-          onClick={() => trackButtonClick("YouTube")}
+          onClick={() =>
+            trackButtonClick("YouTube", "https://youtube.com/@laoficina_music")
+          }
         >
           YouTube
         </a>
         <a
-          href="https://twitch.tv/laoficinamusic"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
           className="link-button"
-          onClick={() => trackButtonClick("Twitch")}
+          onClick={() =>
+            trackButtonClick("Twitch", "https://twitch.tv/laoficinamusic")
+          }
         >
           Twitch
         </a>
         <a
-          href="https://soundcloud.com/laoficina_music"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
           className="link-button"
-          onClick={() => trackButtonClick("SoundCloud")}
+          onClick={() =>
+            trackButtonClick(
+              "SoundCloud",
+              "https://soundcloud.com/laoficina_music"
+            )
+          }
         >
           SoundCloud
         </a>
         <a
-          href="https://podcasters.spotify.com/pod/show/la-oficina-music"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
           className="link-button"
-          onClick={() => trackButtonClick("Podcast")}
+          onClick={() =>
+            trackButtonClick(
+              "Podcast",
+              "https://podcasters.spotify.com/pod/show/la-oficina-music"
+            )
+          }
         >
           Otro día en La Oficina,
           <br />
